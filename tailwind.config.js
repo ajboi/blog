243be948e9f6dc9aeaa3a15c44d76f5af9dfdaa0
ajboi <aj.kannan1101@gmail.com>
2021-05-11 11:10:01 +0530

    Initial commit. Set up postcss and tailwind css. Configured purgecss as a plugin of tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: ['bg-red-500', 'px-4'],
+    }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
